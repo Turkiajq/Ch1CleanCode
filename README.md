07d1287971cd2158a0923d857060c20cec7aa2ac
Turki Alqurashi <turki.alqurashi@tuwaiq.edu.sa>
2021-07-01 13:47:15 +0300

    Refactoring

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Ch1CleanCode
+# Challenge 01
+
+
+Code Smell:  Long method (UpdateQuality()).
+
+Definition: method is long and can be divided into smaller methods .
+
+Solution: taking out the parts of the method that can be brought out independently as IncreaseQuality() and DecreaseQuality(), then calling them from Update whenever needed.  
+
+<hr/>
+
+Code Smell:  unnecessary nested if statements 
+
+Definition: the method UpdateQuality() contains nested if statements.
+
+For example the following code:
+<pre><code>
+if (item.SellIn < 11)
+{
+    if (item.Quality < 50)
+    {
+        item.Quality = item.Quality + 1;
+    }
+}
+
+if (item.SellIn < 6)
+{
+    if (item.Quality < 50)
+    {
+        item.Quality = item.Quality + 1;
+    }
+}
+</code></pre>
+
+Solution: The above example Can be simplfied to:
+<pre><code>
+if (item.SellIn < 11 && item.Quality < 50)
+{
+    item.Quality = item.Quality + 1;
+}
+
+if (item.SellIn < 6 && item.Quality < 50)
+{
+        item.Quality = item.Quality + 1;
+}
+</code></pre>
---
+++ csharp/GildedRose.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace csharp
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+        Quality quality = new Quality();
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+            foreach (var item in Items)
+            {
+                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    quality.DecreaseQuality(item);
+                }
+                else
+                {
+                    quality.IncreaseQuality(item);
+                }
+                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    item.SellIn = item.SellIn - 1;
+                }
+                if (item.SellIn < 0)
+                {
+                    if (quality.QualityIsLow(item))
+                    {
+                        item.Quality = item.Quality + 1;
+                        return;
+                    }
+                    if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        quality.DecreaseQuality(item);
+                    }
+                    else
+                    {
+                        item.Quality = item.Quality - item.Quality;
+                    }
+                }
+            }
+        }
+    }
+}
